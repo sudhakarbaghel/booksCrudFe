ae7a02700fd98cd66660b3813f202abc33a74c20
your name <sudhakarsinghbaghel123321@gmail.com>
2023-06-06 13:45:28 +0530

    optimised app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Popup from './components/popup/Popup';
 import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      {/* <Popup/> */}
     </div>
   );
 }
